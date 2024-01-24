e31a79a9191a1d548b2e9dec56b5625508f00983
itgawri <136715144+itgawri@users.noreply.github.com>
2024-01-24 13:21:05 +0100

    Resources.py

--- Resources.py
+++ Resources.py
@@ -11,19 +11,3 @@ elif location == "Poland" and assessment_score >= 75:
   print("Jest dobrze")
 else:
   print(f"Thank you for your interest, we regret to inform you that we will not be moving forward with your application.")
-
-# PL------------------------------------------------------------------------------------
-
-wynik_oceny = 85
-miejsce = "Polska"
-miejsce2 = "Kanada"
-miejsce3 = "USA"
-
-if miejsce3 == "USA" or miejsce2 == "Kanada" and wynik_oceny >= 75:
-  print(f"Twój wynik oceny to {wynik_oceny}/100! Zostałeś przyjęty!")
-elif miejsce == "USA" and miejsce != "Kanada" and wynik_oceny >= 75:
-  print(f"Przepraszamy, jesteś zarejestrowany w {miejsce}, ale to stanowisko wymaga bycia zarejestrowanym w USA/Kanadzie. Czy jesteś zainteresowany inną rolą?")
-elif miejsce == "Polska" and wynik_oceny >= 75:
-  print("Jest dobrze")
-else:
-  print(f"Dziękujemy za zainteresowanie, niestety informujemy, że nie będziemy kontynuować Twojej aplikacji.")
